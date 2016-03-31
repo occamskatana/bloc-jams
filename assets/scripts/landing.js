47387d0a1259cd05aced07cc2d7dd8c9c884af7d
occamskatana <johngallweycarter@gmail.com>
2016-03-31 14:16:03 -0400

    refactored

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -1,34 +1,12 @@
 var animatePoints = function(){
 	var points = document.getElementsByClassName('point');
 
-	for(var i = 0; i < 3; i++){
-		var revealFirstPoint = function(){
-			var point = points[i];
-			point.style.opacity = 1;
-			point.style.transform = "scaleX(1) translateY(0)";
-			point.style.msTransorm = "scaleX(1) translateY(0)";
-			point.style.WebkitTransform = "scaleX(1) translateY(0)"
-		};
-
-		var revealSecondPoint = function(){
-			var point = points[i];
-			point.style.opacity = 1;
-			point.style.transform = "scaleX(1) translateY(0)";
-			point.style.msTransorm = "scaleX(1) translateY(0)";
-			point.style.WebkitTransform = "scaleX(1) translateY(0)"
-		};
-
-		var revealThirdPoint = function(){
-			var point = points[i];
-			point.style.opacity = 1;
-			point.style.transform = "scaleX(1) translateY(0)";
-			point.style.msTransorm = "scaleX(1) translateY(0)";
-			point.style.WebkitTransform = "scaleX(1) translateY(0)"
-		};
-
-		revealFirstPoint();
-		revealSecondPoint();
-		revealThirdPoint();
+	for(var i = 0; i < 3; i++){	
+		var point = points[i];
+		point.style.opacity = 1;
+		point.style.transform = "scaleX(1) translateY(0)";
+		point.style.msTransorm = "scaleX(1) translateY(0)";
+		point.style.WebkitTransform = "scaleX(1) translateY(0)"		
 	};
 }
 
