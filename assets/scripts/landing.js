3758048f9797479cbe31d1cec7beb387fad1b739
occamskatana <johngallweycarter@gmail.com>
2016-03-31 14:38:57 -0400

    checkpoint down

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function(){
-	var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+
+var animatePoints = function(points){
 	for(var i = 0; i < 3; i++){	
 		var point = points[i];
 		point.style.opacity = 1;
@@ -10,3 +11,20 @@ var animatePoints = function(){
 	};
 }
 
+window.onload = function(){
+
+	//auto-load on tall ass screens
+		if(window.innerHeight > 950){
+			animatePoints(pointsArray);
+		}
+
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+	window.addEventListener('scroll', function(event){
+		if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+			animatePoints(pointsArray);
+		}
+
+	});
+}
