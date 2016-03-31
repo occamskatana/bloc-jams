0fd4f1b9c49581598d21a6e0407914b759d5d43f
occamskatana <johngallweycarter@gmail.com>
2016-03-31 14:08:07 -0400

    assignment_22 really done this time

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -1,32 +1,34 @@
 var animatePoints = function(){
 	var points = document.getElementsByClassName('point');
-	var revealFirstPoint = function(){
-		var point = points[0];
-		point.style.opacity = 1;
-		point.style.transform = "scaleX(1) translateY(0)";
-		point.style.msTransorm = "scaleX(1) translateY(0)";
-		point.style.WebkitTransform = "scaleX(1) translateY(0)"
+
+	for(var i = 0; i < 3; i++){
+		var revealFirstPoint = function(){
+			var point = points[i];
+			point.style.opacity = 1;
+			point.style.transform = "scaleX(1) translateY(0)";
+			point.style.msTransorm = "scaleX(1) translateY(0)";
+			point.style.WebkitTransform = "scaleX(1) translateY(0)"
+		};
+
+		var revealSecondPoint = function(){
+			var point = points[i];
+			point.style.opacity = 1;
+			point.style.transform = "scaleX(1) translateY(0)";
+			point.style.msTransorm = "scaleX(1) translateY(0)";
+			point.style.WebkitTransform = "scaleX(1) translateY(0)"
+		};
+
+		var revealThirdPoint = function(){
+			var point = points[i];
+			point.style.opacity = 1;
+			point.style.transform = "scaleX(1) translateY(0)";
+			point.style.msTransorm = "scaleX(1) translateY(0)";
+			point.style.WebkitTransform = "scaleX(1) translateY(0)"
+		};
+
+		revealFirstPoint();
+		revealSecondPoint();
+		revealThirdPoint();
 	};
+}
 
-	var revealSecondPoint = function(){
-		var point = points[1];
-		point.style.opacity = 1;
-		point.style.transform = "scaleX(1) translateY(0)";
-		point.style.msTransorm = "scaleX(1) translateY(0)";
-		point.style.WebkitTransform = "scaleX(1) translateY(0)"
-	};
-
-	var revealThirdPoint = function(){
-		var point = points[2];
-		point.style.opacity = 1;
-		point.style.transform = "scaleX(1) translateY(0)";
-		point.style.msTransorm = "scaleX(1) translateY(0)";
-		point.style.WebkitTransform = "scaleX(1) translateY(0)"
-	};
-
-	revealFirstPoint();
-	revealSecondPoint();
-	revealThirdPoint();
-};
-
-animatePoints();
