7b2fa2911252008addcdd42cb17138c9eec314ed
occamskatana <johngallweycarter@gmail.com>
2016-03-31 22:02:02 -0400

    checkpoint really done this time

--- assets/scripts/collection.js
+++ assets/scripts/collection.js
@@ -1,23 +1,24 @@
-var collectionItemTemplate = 
-	'<div class="collection-album-container column fourth">'
-	+	'<img src="assets/images/album_covers/01.png"/>'
-	+	'<div class="collection-album-info caption">'
-	+		'<p>'
-	+			'<a class="album-name" href="album.html">The Colors</a>'
-	+			'<br />'
-	+			'<a href="album.html">Pablo Picasso</a>'
-	+			'<br />'
-	+			'X songs'
-	+			'<br />'
-	+		'</p>'
-	+	'</div>'
-	+'</div>'
-;
+var collectionItemTemplate =
+     '<div class="collection-album-container column fourth">'
+   + '  <img src="assets/images/album_covers/01.png"/>'
+   + '  <div class="collection-album-info caption">'
+   + '    <p>'
+   + '      <a class="album-name" href="/album.html"> The Colors </a>'
+   + '      <br/>'
+   + '      <a href="/album.html"> Pablo Picasso </a>'
+   + '      <br/>'
+   + '      X songs'
+   + '      <br/>'
+   + '    </p>'
+   + '  </div>'
+   + '</div>'
+   ;
 
-window.onLoad = function() {
+window.onload = function() {
 	var collectionContainer = document.getElementsByClassName('album-covers')[0];
-	collectionContainer.innerHtml = '';
+	console.log(collectionContainer)
+	collectionContainer.innerHTML = '';
 	for(var i = 0; i < 12; i++){
-		collectionContainer.innerHtml += collectionItemTemplate;
-	}
+		collectionContainer.innerHTML += collectionItemTemplate;
+	};
 } 
