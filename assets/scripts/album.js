d1fc85b0fc147f9c44ae9ec509934f3b4b3345b8
occamskatana <johngallweycarter@gmail.com>
2016-04-04 20:51:53 -0400

    checkpoint 28 complete

--- assets/scripts/album.js
+++ assets/scripts/album.js
@@ -61,8 +61,8 @@ var albumPicasso = {
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
-var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playButtonTemplate = '<a class="album-song-button"><span class="icon ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="icon ion-pause"></span></a>';
 var currentlyPlayingSong = null;
 
  window.onload = function() {
